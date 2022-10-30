a310e7cca711524ac771e1dd619a115ab101cc1f
HedvikaCapkova <hedvika.capkova@gmail.com>
2022-10-30 23:27:11 +0100

    click in component

--- 221025_lekce_6/zarovky/index.js
+++ 221025_lekce_6/zarovky/index.js
@@ -5,9 +5,16 @@ appElm.append(Bulb(true), Bulb(), Bulb(true));
 
 const clickBulb = document.querySelectorAll('.bulb');
 
-clickBulb.forEach((element) => {
-    element.onclick = () => {
-        element.classList.toggle("bulb--on");
-    }
-});
+// clickBulb.forEach((element) => {
+//     element.onclick = () => {
+//         element.classList.toggle("bulb--on");
+//     }
+// });
+
+
+// appElm.addEventListener("click", (e) => {
+//     clickBulb.forEach(element => {
+//         e.target === element && element.classList.toggle("bulb--on");
+//     })
+// })
 
--- 221025_lekce_6/zarovky/komponenta/index.js
+++ 221025_lekce_6/zarovky/komponenta/index.js
@@ -2,9 +2,12 @@ export const Bulb = (isOn) => {
     
     const bulbElm = document.createElement('div');
     bulbElm.classList.add('bulb');
+    bulbElm.onclick = () => myClick(bulbElm);
     isOn && bulbElm.classList.add('bulb--on');
     
     return bulbElm;
 };
 
-
+const myClick = (element) => {
+    element.classList.toggle("bulb--on");
+}
