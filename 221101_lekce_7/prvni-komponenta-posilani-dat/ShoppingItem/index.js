32fef61c4344a419811576f49a928980f1a5b421
HedvikaCapkova <hedvika.capkova@gmail.com>
2022-11-05 20:03:13 +0100

    update input

--- 221101_lekce_7/prvni-komponenta-posilani-dat/ShoppingItem/index.js
+++ 221101_lekce_7/prvni-komponenta-posilani-dat/ShoppingItem/index.js
@@ -6,8 +6,6 @@ export const ShoppingItem = (props) => {
     checkClass = 'item__btn-done--check';
   }
 
-  
-
   const element = document.createElement('li');
   element.classList.add('item');
   element.innerHTML = `
@@ -15,9 +13,12 @@ export const ShoppingItem = (props) => {
     <div class="item__amount">${amount} ${unit}</div>
     <button class="item__btn-done ${checkClass}"></button>
   `;
+
+  const inputElm = document.querySelector(".item__name");
+  inputElm.addEventListener("change", () => {
+    console.log(inputElm);
+  });
   
-  const inputValue = document.querySelector(".item__name");
-  console.log(inputValue);
 
   element.querySelector('button').addEventListener('click', () => {
     fetch(`https://apps.kodim.cz/daweb/shoplist/api/weeks/8/days/mon/${id}`, {
