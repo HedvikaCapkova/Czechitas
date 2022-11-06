355dfc72ee3ecfb73d1a66d424ea5ce6c532ed03
HedvikaCapkova <hedvika.capkova@gmail.com>
2022-11-06 14:15:15 +0100

    complete

--- 221101_lekce_7/prvni-komponenta-posilani-dat/ShoppingItem/index.js
+++ 221101_lekce_7/prvni-komponenta-posilani-dat/ShoppingItem/index.js
@@ -1,33 +1,26 @@
 export const ShoppingItem = (props) => {
   const { id, product, amount, unit, done } = props;
 
-  let checkClass = '';
-  if (done) {
-    checkClass = 'item__btn-done--check';
-  }
-
   const element = document.createElement('li');
   element.classList.add('item');
   element.innerHTML = `
-    <input type="number" class="item__name"/>${product}
+    <input type="number" min="0" class="item__name" value="${amount}"/>
+    <div class="product">${product}</div>
     <div class="item__amount">${amount} ${unit}</div>
-    <button class="item__btn-done ${checkClass}"></button>
+    <button class="item__btn-done ${done && 'item__btn-done--check'}"></button>
   `;
 
-  element.querySelector(".item__name").addEventListener("change", () => {
-    console.log(element.value);
-
+  element.querySelector(".item__name").addEventListener("change", (event) => {
     fetch(`https://apps.kodim.cz/daweb/shoplist/api/weeks/8/days/mon/${id}`, {
       method: 'PATCH',
       headers: {
         'Content-Type': 'application/json',
       },
-      body: JSON.stringify({ amount: element.value }),
+      body: JSON.stringify({ amount: Number(event.target.value) }),
     }).then((response) => response.json())
       .then((data) => element.replaceWith(ShoppingItem(data.results)));
   });
   
-
 
   element.querySelector('button').addEventListener('click', () => {
     fetch(`https://apps.kodim.cz/daweb/shoplist/api/weeks/8/days/mon/${id}`, {
@@ -41,7 +34,5 @@ export const ShoppingItem = (props) => {
   });
 
   return element;
-
-  
 };
 
