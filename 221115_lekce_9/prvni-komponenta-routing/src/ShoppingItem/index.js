9c28ecfa759719c2769b376054a25d36cb1a526a
HedvikaCapkova <hedvika.capkova@gmail.com>
2022-11-15 21:05:54 +0100

    lekce 9

---
+++ 221115_lekce_9/prvni-komponenta-routing/src/ShoppingItem/index.js
@@ -0,0 +1,40 @@
+import './style.css';
+
+export const ShoppingItem = (props) => {
+  const { item, day, onDelete } = props;
+  const { id, product, amount, unit, done } = item;
+
+  let checkClass = 'icon-nocheck';
+  if (done) {
+    checkClass = 'icon-check';
+  }
+
+  const element = document.createElement('li');
+  element.classList.add('item');
+  element.innerHTML = `
+    <div class="item__name">${product}</div>
+    <div class="item__amount">${amount} ${unit}</div>
+    <button class="btn-check item__btn ${checkClass}"></button>
+    <button class="btn-delete item__btn icon-cross"></button>
+  `;
+  element.querySelector('.btn-check').addEventListener('click', () => {
+    fetch(`https://apps.kodim.cz/daweb/shoplist/api/me/week/${day}/items/${id}`, {
+      method: 'PATCH',
+      headers: {
+        'Authorisation': 'Basic hedvika.capkova@gmail.com:12345678',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ done: !done }),
+    }).then((response) => response.json())
+      .then((data) => element.replaceWith(ShoppingItem({ 
+        item: data.results,
+        day: day,
+        onDelete: onDelete,
+      })));
+  });
+  element.querySelector('.btn-delete').addEventListener('click', () => {
+    onDelete(id);
+  });
+
+  return element;
+};
---
+++ 221115_lekce_9/prvni-komponenta-routing/src/ShoppingList/index.js
@@ -0,0 +1,55 @@
+import { ShoppingItem } from "../ShoppingItem/index.js";
+import './style.css';
+
+export const ShoppingList = (props) => {
+  const { day, dayName, items } = props;
+
+  const element = document.createElement('div');
+  element.classList.add('shopping-list');
+  element.innerHTML = `
+    <h2>${dayName}</h2>
+    <ul class="shopping-list__items"></ul>
+  `;
+  
+  if (items === undefined) {
+    fetch(`https://apps.kodim.cz/daweb/shoplist/api/me/week/${day}`, {
+      method: 'GET',
+      headers: {
+        'Authorisation': 'Basic hedvika.capkova@gmail.com:12345678',
+      }
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        element.replaceWith(ShoppingList({
+          day: day,
+          dayName: dayName,
+          items: data.results.items,
+        }));
+      });
+
+    return element;
+  } 
+  
+  const handleDelete = (itemId) => {
+    fetch(`https://apps.kodim.cz/daweb/shoplist/api/me/week/${day}/items/${itemId}`, {
+      method: 'DELETE',
+      headers: {
+        'Authorisation': 'Basic hedvika.capkova@gmail.com:12345678',
+      }
+    }).then((response) => response.json())
+      .then((data) => element.replaceWith(ShoppingList({
+        day: day,
+        dayName: dayName,
+        items: data.results.items,
+      })));
+  }
+  
+  const ulElement = element.querySelector('ul');
+  ulElement.append(...items.map((item) => ShoppingItem({ 
+    item: item,
+    day: day,
+    onDelete: handleDelete,
+  })));
+
+  return element;
+};
