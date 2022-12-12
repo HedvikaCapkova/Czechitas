095053331f7f6d8536d9c3d240393628f5becadf
HedvikaCapkova <hedvika.capkova@gmail.com>
2022-12-12 22:17:27 +0100

    commit

---
+++ 221212_Advent_Of_Code/skupiny.js
@@ -0,0 +1,17 @@
+import fs from 'fs';
+
+const lines = fs.readFileSync('skupiny.txt', 'utf-8').split('\n');
+
+const groups = [];
+let group = [];
+
+for (const line of lines){
+    if (line === '') {
+        groups.push(group);
+        group = [];
+    } else {
+        group.push(line);
+    }
+}
+
+ console.log(groups);
